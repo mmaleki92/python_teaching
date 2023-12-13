6de028bc8a04730aadd05c591a6ed52078085b92
Morteza <maleki.morteza92@gmail.com>
2023-12-13 12:29:45 +0330

    library

--- projects/pyqt6_projects/library management system/db_model.py
+++ projects/pyqt6_projects/library management system/db_model.py
@@ -35,8 +35,16 @@ def init_db():
             book_id INTEGER,
             member_id INTEGER,
             due_date DATE,
+            returned_date DATE,
             FOREIGN KEY (book_id) REFERENCES books (book_id),
             FOREIGN KEY (member_id) REFERENCES members (member_id)
+        )
+    ''')
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS inside_outside (
+            book_id INTEGER PRIMARY KEY,
+            status TEXT CHECK( status IN ('inside','outside') ) NOT NULL DEFAULT 'inside',
+            FOREIGN KEY (book_id) REFERENCES books (book_id)
         )
     ''')
 
@@ -44,24 +52,81 @@ def init_db():
     conn.close()
 
 
-def add_book_to_db(title):
+def add_book_to_db(title, author, isbn, quantity):
     conn = sqlite3.connect('library.db')
     cursor = conn.cursor()
-    cursor.execute('INSERT INTO books (title) VALUES (?)', (title,))
+
+    # Assuming you have a way to handle or add authors in your authors table, get the author_id
+    # Here's a simple way to get or add an author (requires modification based on your schema)
+    cursor.execute('SELECT author_id FROM authors WHERE name = ?', (author,))
+    author_id = cursor.fetchone()
+    if not author_id:
+        cursor.execute('INSERT INTO authors (name) VALUES (?)', (author,))
+        author_id = cursor.lastrowid
+    else:
+        author_id = author_id[0]
+
+    # Now insert the book details
+    cursor.execute('INSERT INTO books (title, author_id, isbn, quantity) VALUES (?, ?, ?, ?)', 
+                   (title, author_id, isbn, quantity))
+
+    # Add an entry to inside_outside table
+    book_id = cursor.lastrowid
+    cursor.execute('INSERT INTO inside_outside (book_id, status) VALUES (?, "inside")', (book_id,))
+
     conn.commit()
     conn.close()
+
+
+
 
 def get_all_books():
     conn = sqlite3.connect('library.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT book_id, title FROM books')
+    cursor.execute('''
+        SELECT books.book_id, books.title, authors.name, books.isbn, books.quantity 
+        FROM books 
+        LEFT JOIN authors ON books.author_id = authors.author_id
+    ''')
     books = cursor.fetchall()
     conn.close()
     return books
+
 
 def delete_book_from_db(book_id):
     conn = sqlite3.connect('library.db')
     cursor = conn.cursor()
     cursor.execute('DELETE FROM books WHERE book_id = ?', (book_id,))
+    conn.commit()
+    conn.close()
+
+def borrow_book(book_id, member_id, due_date):
+    conn = sqlite3.connect('library.db')
+    cursor = conn.cursor()
+
+    # First, check if the book is already borrowed
+    cursor.execute('SELECT status FROM inside_outside WHERE book_id = ?', (book_id,))
+    status_result = cursor.fetchone()
+    if status_result and status_result[0] == 'outside':
+        conn.close()
+        return "Book is already borrowed"
+
+    # Proceed to borrow the book
+    cursor.execute('UPDATE inside_outside SET status = "outside" WHERE book_id = ?', (book_id,))
+    cursor.execute('INSERT INTO loans (book_id, member_id, due_date) VALUES (?, ?, ?)', (book_id, member_id, due_date))
+    conn.commit()
+    conn.close()
+    return "Book borrowed successfully"
+
+def return_book(book_id):
+    conn = sqlite3.connect('library.db')
+    cursor = conn.cursor()
+
+    # Update the loans table to set the returned_date to today
+    cursor.execute('UPDATE loans SET returned_date = CURRENT_DATE WHERE book_id = ? AND returned_date IS NULL', (book_id,))
+
+    # Update the inside_outside table to set the status back to 'inside'
+    cursor.execute('UPDATE inside_outside SET status = "inside" WHERE book_id = ?', (book_id,))
+
     conn.commit()
     conn.close()
--- projects/pyqt6_projects/library management system/main.py
+++ projects/pyqt6_projects/library management system/main.py
@@ -1,6 +1,73 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QLineEdit, QListWidget, QMessageBox
-from db_model import init_db, add_book_to_db, get_all_books, delete_book_from_db
+from db_model import init_db, add_book_to_db, get_all_books, delete_book_from_db, borrow_book, return_book
+from PyQt5.QtWidgets import QLabel, QDateEdit, QDateTimeEdit
+from PyQt5.QtWidgets import QGroupBox, QGridLayout
+from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
+
+from datetime import datetime
+
+from PyQt5.QtWidgets import QDialog, QFormLayout, QDialogButtonBox
+
+class AddBookDialog(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("Add New Book")
+        self.layout = QFormLayout(self)
+
+        # Book details fields
+        self.titleInput = QLineEdit(self)
+        self.layout.addRow("Title:", self.titleInput)
+        
+        self.authorInput = QLineEdit(self)
+        self.layout.addRow("Author:", self.authorInput)
+
+        self.isbnInput = QLineEdit(self)
+        self.layout.addRow("ISBN:", self.isbnInput)
+
+        self.quantityInput = QLineEdit(self)
+        self.layout.addRow("Quantity:", self.quantityInput)
+
+        # Dialog buttons
+        self.buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+        self.layout.addWidget(self.buttons)
+
+    def getBookDetails(self):
+        return (self.titleInput.text(), self.authorInput.text(), self.isbnInput.text(), self.quantityInput.text())
+
+class BorrowBookDialog(QDialog):
+    def __init__(self, selected_book_title, book_ids, parent=None):
+        super().__init__(parent)
+        self.selected_book_title = selected_book_title
+        self.book_ids = book_ids  # This should be a dictionary
+        self.setWindowTitle("Borrow Book")
+        self.layout = QFormLayout(self)
+
+        # Borrower ID and Due Date
+        self.memberIdInput = QLineEdit(self)
+        self.dueDateInput = QDateEdit(self)
+        self.dueDateInput.setCalendarPopup(True)
+        self.dueDateInput.setDateTime(datetime.now())
+
+        self.layout.addRow("Member ID:", self.memberIdInput)
+        self.layout.addRow("Due Date:", self.dueDateInput)
+
+        self.book_ids = book_ids  # Dictionary of book titles to IDs
+
+        # Dialog buttons
+        self.buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
+        self.buttons.accepted.connect(self.accept)
+        self.buttons.rejected.connect(self.reject)
+        self.layout.addWidget(self.buttons)
+
+    def getBorrowDetails(self):
+        book_id = self.book_ids.get(self.selected_book_title)  # Safely get the book ID
+        member_id = self.memberIdInput.text()
+        due_date = self.dueDateInput.date().toString("yyyy-MM-dd")
+        return (book_id, member_id, due_date)
+
 
 class LibrarySystem(QWidget):
     def __init__(self):
@@ -12,30 +79,52 @@ class LibrarySystem(QWidget):
 
     def initUI(self):
         self.setWindowTitle('Library Management System')
-        self.setGeometry(100, 100, 400, 300)
-
+        self.setGeometry(100, 100, 600, 400)
         layout = QVBoxLayout()
 
-        # Book Title Input
-        self.bookTitleInput = QLineEdit(self)
-        self.bookTitleInput.setPlaceholderText('Enter book title')
-        layout.addWidget(self.bookTitleInput)
+        # Group Box for Book Details
+        bookDetailsGroup = QGroupBox("Book Details")
+        bookDetailsLayout = QGridLayout()
 
-        # Add Button
-        self.addButton = QPushButton('Add Book', self)
-        self.addButton.clicked.connect(self.addBook)
-        layout.addWidget(self.addButton)
+        # Add Book Dialog Button
+        self.addBookDialogButton = QPushButton('Add New Book', self)
+        self.addBookDialogButton.clicked.connect(self.openAddBookDialog)
+        layout.addWidget(self.addBookDialogButton)
 
-        # List of Books
-        self.booksList = QListWidget(self)
-        layout.addWidget(self.booksList)
+        # Borrow Book Dialog Button
+        self.borrowBookDialogButton = QPushButton('Borrow Book', self)
+        self.borrowBookDialogButton.clicked.connect(self.openBorrowBookDialog)
+        layout.addWidget(self.borrowBookDialogButton)
 
-        # Delete Button
         self.deleteButton = QPushButton('Delete Selected Book', self)
         self.deleteButton.clicked.connect(self.deleteBook)
         layout.addWidget(self.deleteButton)
 
+
         self.setLayout(layout)
+        # Return Book Button
+        self.returnBookButton = QPushButton('Return Selected Book', self)
+        self.returnBookButton.clicked.connect(self.returnBook)
+        layout.addWidget(self.returnBookButton)
+
+        # Initialize the table
+        self.booksTable = QTableWidget(self)
+        self.booksTable.setColumnCount(4)  # For title, author, ISBN, quantity
+        self.booksTable.setHorizontalHeaderLabels(["Title", "Author", "ISBN", "Quantity"])
+        self.booksTable.horizontalHeader().setStretchLastSection(True)
+        layout.addWidget(self.booksTable)
+
+    def returnBook(self):
+        selected_row = self.booksTable.currentRow()
+        if selected_row != -1:
+            book_title = self.booksTable.item(selected_row, 0).text()
+            book_id = self.book_ids[book_title]
+
+            return_book(book_id)
+            QMessageBox.information(self, 'Return Book', 'Book returned successfully')
+            self.loadBooks()
+        else:
+            QMessageBox.information(self, 'Error', 'Please select a book to return')
 
     def addBook(self):
         book_title = self.bookTitleInput.text()
@@ -47,11 +136,17 @@ class LibrarySystem(QWidget):
             QMessageBox.information(self, 'Error', 'Please enter a book title')
 
     def loadBooks(self):
-        self.booksList.clear()
-        self.book_ids.clear()
-        for book_id, title in get_all_books():
-            self.booksList.addItem(title)
-            self.book_ids[title] = book_id  # Map the title to its book ID
+        self.booksTable.setRowCount(0)  # Clear the table
+        for book_id, title, author, isbn, quantity in get_all_books():
+            row_position = self.booksTable.rowCount()
+            self.booksTable.insertRow(row_position)
+
+            self.booksTable.setItem(row_position, 0, QTableWidgetItem(title))
+            self.booksTable.setItem(row_position, 1, QTableWidgetItem(author))
+            self.booksTable.setItem(row_position, 2, QTableWidgetItem(isbn))
+            self.booksTable.setItem(row_position, 3, QTableWidgetItem(str(quantity)))
+
+            self.book_ids[title] = book_id  # Use title as key; consider using book_id for uniqueness
 
     def deleteBook(self):
         selected_item = self.booksList.currentItem()
@@ -62,6 +157,45 @@ class LibrarySystem(QWidget):
             self.loadBooks()
         else:
             QMessageBox.information(self, 'Error', 'Please select a book to delete')
+
+    def borrowBook(self):
+        selected_row = self.booksTable.currentRow()
+        if selected_row != -1:
+            book_title = self.booksTable.item(selected_row, 0).text()  # Assuming the first column is the title
+            book_id = self.book_ids[book_title]
+            member_id = self.memberIdInput.text()
+            due_date = self.dueDateInput.date().toString("yyyy-MM-dd")
+
+            book_id = int(book_id)
+
+            response = borrow_book(book_id, member_id, due_date)
+            QMessageBox.information(self, 'Borrow Book', response)
+            self.loadBooks()
+        else:
+            QMessageBox.information(self, 'Error', 'Please select a book to borrow')
+
+
+    def openAddBookDialog(self):
+        dialog = AddBookDialog(self)
+        if dialog.exec_():
+            title, author, isbn, quantity = dialog.getBookDetails()
+            add_book_to_db(title, author, isbn, quantity)  # Pass all details to the function
+            self.loadBooks()
+
+    def openBorrowBookDialog(self):
+        selected_row = self.booksTable.currentRow()
+        if selected_row != -1:
+            selected_book_title = self.booksTable.item(selected_row, 0).text()
+            dialog = BorrowBookDialog(selected_book_title, self.book_ids, self)
+
+            if dialog.exec_():
+                book_id, member_id, due_date = dialog.getBorrowDetails()
+                book_id = int(book_id)
+                response = borrow_book(book_id, member_id, due_date)
+                QMessageBox.information(self, 'Borrow Book', response)
+                self.loadBooks()
+            else:
+                QMessageBox.information(self, 'Error', 'Please select a book to borrow')
 
 def main():
     app = QApplication(sys.argv)
