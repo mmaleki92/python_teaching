57b3f59468e6cb569318e4062f5ea64dbaf15471
Morteza Maleki <maleki.morteza92@gmail.com>
2024-01-12 16:20:48 +0330

    added a ghost

--- MFT_class/game_01.py
+++ MFT_class/game_01.py
@@ -4,12 +4,18 @@ pygame.init()
 
 RED = (255, 0, 0)
 BLACK = (0, 0, 0)
+
+G_color = (255, 255, 0)
+
 dis = pygame.display.set_mode((400,300))
 clock = pygame.time.Clock()
 game = True
 x, y = 10, 10
-x_change, y_change = 0, 0
 
+x_g, y_g = 400 - 10, 300 - 10
+
+x_change, y_change = 0, 0
+size_x, size_y = 50, 50
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,23 +45,43 @@ while game:
     x = x + x_change
     y = y + y_change
 
+    x_g = x_g - x_change
+    y_g = y_g - y_change
+    
     if x>400:
         x = 0
+        size_x = size_x + 5
     elif x<0:
         x = 400
+        size_x = size_x - 5
     if y>300:
         y = 0
+        size_y = size_y + 5
     elif y<0:
         y = 300
+        size_y = size_y - 5
+
+    if x_g>400:
+        x_g = 0
+    elif x_g<0:
+        x_g = 400
+
+    if y_g>300:
+        y_g = 0
+    elif y_g<0:
+        y_g = 300
+
 
     dis.fill(BLACK)
 
+    pygame.draw.rect(dis, RED, [x, y, size_x, size_y])
+    pygame.draw.rect(dis, G_color, [x_g, y_g, 50, 50])
 
-    pygame.draw.rect(dis, RED, [x, y, 50, 50])
+
     pygame.display.update()
-    
-   # x_change, y_change = 0, 0
 
-    clock.tick(30)
+    # x_change, y_change = 0, 0
+
+    clock.tick(40)
 pygame.quit()
 quit()
